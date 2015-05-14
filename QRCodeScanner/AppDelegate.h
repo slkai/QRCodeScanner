125d9b09d672513ba496b155ca7e4bad8c45a83f
dengjunjie <dengjunjie@MacMini.local>
2015-05-14 17:03:36 +0800

    Initial Commit

---
+++ QRCodeScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeScanner
+//
+//  Created by dengjunjie on 15/5/14.
+//  Copyright (c) 2015年 dengjunjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCodeScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeScanner
+//
+//  Created by dengjunjie on 15/5/14.
+//  Copyright (c) 2015年 dengjunjie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
